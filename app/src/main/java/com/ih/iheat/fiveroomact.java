7146ea8ab1cd1bab6c470f4af132276aca9de581
bigsupersquid <0.uradork.0@gmail.com>
2014-12-13 14:16:17 -0600

    messed up

--- app/src/main/java/com/ih/iheat/fiveroomact.java
+++ app/src/main/java/com/ih/iheat/fiveroomact.java
@@ -1,68 +1,75 @@
 package com.ih.iheat;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.graphics.Matrix;
-import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
-public class fiveroomact extends Activity {
-	ImageView iv[]=new ImageView[5];
-    ImageView degree[]=new ImageView[5];
-    TextView onoff[]=new TextView[5];
-    TextView temp[]=new TextView[5];
-    TextView cent[]=new TextView[5];
-    TextView roomtemp[]=new TextView[5];
+public class fiveroomact extends Activity implements OnClickListener {
+	View iv[]=new View[6];
+	ImageView degree[]=new ImageView[6];
+	TextView onoff[]=new TextView[6];
+	TextView temp[]=new TextView[6];
+	TextView cent[]=new TextView[6];
+	TextView roomtemp[]=new TextView[6];
+	String devarray[];
 	ImageView setup;
 	String email;
-    @Override
+	String status[]=new String[6];
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fiveroomlayout);
+		setContentView(R.layout.fourroomlayout);
+		email=getIntent().getStringExtra("email");
+		iv[0]=(View)findViewById(R.id.view1);
+		degree[0]=(ImageView)findViewById(R.id.imageViewDegree1);
+		onoff[0]=(TextView)findViewById(R.id.textViewOnOff1);
+		temp[0]=(TextView)findViewById(R.id.textViewTempRoom1);
+		cent[0]=(TextView)findViewById(R.id.textViewCent1);
+		roomtemp[0]=(TextView)findViewById(R.id.textViewRoomTemp1);
+		iv[1]=(View)findViewById(R.id.view2);
+		degree[1]=(ImageView)findViewById(R.id.imageViewDegree2);
+		onoff[1]=(TextView)findViewById(R.id.textViewOnOff2);
+		temp[1]=(TextView)findViewById(R.id.textViewTemp2);
+		cent[1]=(TextView)findViewById(R.id.textViewCent2);
+		roomtemp[1]=(TextView)findViewById(R.id.textViewRoomTemp2);
+		
+		iv[2]=(View)findViewById(R.id.view3);
+		degree[2]=(ImageView)findViewById(R.id.imageViewDegree3);
+		onoff[2]=(TextView)findViewById(R.id.textViewOnOff3);
+		temp[2]=(TextView)findViewById(R.id.textViewTemp3);
+		cent[2]=(TextView)findViewById(R.id.textViewCent3);
+		roomtemp[2]=(TextView)findViewById(R.id.textViewRoomTemp3);
+		
+		iv[3]=(View)findViewById(R.id.view4);
+		degree[3]=(ImageView)findViewById(R.id.imageViewDegree4);
+		onoff[3]=(TextView)findViewById(R.id.textViewOnOff4);
+		temp[3]=(TextView)findViewById(R.id.textViewTemp4);
+		cent[3]=(TextView)findViewById(R.id.textViewCent4);
+		roomtemp[3]=(TextView)findViewById(R.id.textViewRoomTemp4);
+		
+		iv[4]=(View)findViewById(R.id.view5);
+		degree[4]=(ImageView)findViewById(R.id.imageViewDegree5);
+		onoff[4]=(TextView)findViewById(R.id.textViewOnOff5);
+		temp[4]=(TextView)findViewById(R.id.textViewTemp5);
+		cent[4]=(TextView)findViewById(R.id.textViewCent5);
+		roomtemp[4]=(TextView)findViewById(R.id.textViewRoomTemp5);
 
-		iv[0]=(ImageView)findViewById(R.id.imageView1);
-        degree[0]=(ImageView)findViewById(R.id.imageViewDegree1);
-        onoff[0]=(TextView)findViewById(R.id.textViewOnOff1);
-        temp[0]=(TextView)findViewById(R.id.textViewTempRoom1);
-        cent[0]=(TextView)findViewById(R.id.textViewCent1);
-        roomtemp[0]=(TextView)findViewById(R.id.textViewRoomTemp1);
-
-        iv[1]=(ImageView)findViewById(R.id.imageView2);
-        degree[1]=(ImageView)findViewById(R.id.imageViewDegree2);
-        onoff[1]=(TextView)findViewById(R.id.textViewOnOff2);
-        temp[1]=(TextView)findViewById(R.id.textViewTemp2);
-        cent[1]=(TextView)findViewById(R.id.textViewCent2);
-        roomtemp[1]=(TextView)findViewById(R.id.textViewRoomTemp2);
-
-		iv[2]=(ImageView)findViewById(R.id.imageView3);
-        degree[2]=(ImageView)findViewById(R.id.imageViewDegree3);
-        onoff[2]=(TextView)findViewById(R.id.textViewOnOff3);
-        temp[2]=(TextView)findViewById(R.id.textViewTemp3);
-        cent[2]=(TextView)findViewById(R.id.textViewCent3);
-        roomtemp[2]=(TextView)findViewById(R.id.textViewRoomTemp3);
-
-		iv[3]=(ImageView)findViewById(R.id.imageView4);
-        degree[3]=(ImageView)findViewById(R.id.imageViewDegree4);
-        onoff[3]=(TextView)findViewById(R.id.textViewOnOff4);
-        temp[3]=(TextView)findViewById(R.id.textViewTemp4);
-        cent[3]=(TextView)findViewById(R.id.textViewCent4);
-        roomtemp[3]=(TextView)findViewById(R.id.textViewRoomTemp4);
-
-		iv[4]=(ImageView)findViewById(R.id.imageView5);
-        degree[4]=(ImageView)findViewById(R.id.imageViewDegree5);
-        onoff[4]=(TextView)findViewById(R.id.textViewOnOff5);
-        temp[4]=(TextView)findViewById(R.id.textViewTemp5);
-        cent[4]=(TextView)findViewById(R.id.textViewCent5);
-        roomtemp[4]=(TextView)findViewById(R.id.textViewRoomTemp5);
-
-        email=getIntent().getStringExtra("email");
-
+		for(int i=0;(i<status.length)&&(i<5);i++)
+		{
+		status[i]="0";
+            try {
+                iv[i].setOnClickListener(this);
+            } catch (Exception e) {
+                Log.d("fiveroomact", e.toString());
+            }
+		}
 		setup=(ImageView)findViewById(R.id.imageViewSetup);
 		setup.setOnClickListener(new OnClickListener() {
 			
@@ -73,45 +80,110 @@ public class fiveroomact extends Activity {
 				startActivity(i);
 			}
 		});
+		final ProgressDialog pd = new ProgressDialog(this);
+		pd.setMessage("loading");
+		pd.show();
 		Thread th=new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				String u="?email="+email;
-				final String  result[]=Connection.GET("http://www.thermostat.ie/test/androheatstat.jsp"+u).split("%");
+				String total[]=Connection.GET("http://www.thermostat.ie/test/androheatstat.jsp"+u).split("~~~");
+				final String  result[]=total[0].split("%");
+				devarray=total[1].split("%");
+				status=result;
 				fiveroomact.this.runOnUiThread(new Runnable() {
 		public void run()
 		{
 			int i=0;
-			while(i<result.length)
+			while((i<result.length)&&(i<5))
 			{
 				String r=result[i];
-				if(i>= iv.length)
-					break;
 					if(r.equals("0"))
 					{
-						//iv[i].setBackgroundResource(R.drawable.hotroom4);
+//                        if (i < 2)
+//                            iv[i].setBackgroundResource(R.drawable.cold_small);
+//                        else
+//                            iv[i].setBackgroundResource(R.drawable.cold_smaller);
 					}
 					else
-						{
-							if(i==0)
-							iv[i].setBackgroundResource(R.drawable.hot_small);
-							else
-							iv[i].setBackgroundResource(R.drawable.hot_smaller);
+					{
+					if (i < 2)
+
+                    	iv[i].setBackgroundResource(R.drawable.hot_small);
+
+                    else
+
+                    	iv[i].setBackgroundResource(R.drawable.hot_smaller);
+
 //						roomtemp[i].setVisibility(View.INVISIBLE);
 //						temp[i].setVisibility(View.INVISIBLE);
 						onoff[i].setText("Heating On");
 //						degree[i].setVisibility(View.INVISIBLE);
 //						cent[i].setVisibility(View.INVISIBLE);
+						}
 			i++;
 			}
+			pd.hide();
 		}
+		
 				});
 	}
 });
 		th.start();
 	}
-
-
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		if(arg0==iv[0])
+		{
+		Intent i=new Intent(this,roomdetailact.class);
+		i.putExtra("status",status[0]);
+		i.putExtra("roomno","1");
+		i.putExtra("email",email);
+		i.putExtra("devid",devarray[0]);
+		startActivity(i);
+		}
+		else if(arg0==iv[1])
+		{
+			Intent i=new Intent(this,roomdetailact.class);
+			System.out.println("status "+status[1]);
+			i.putExtra("status",status[1]);
+			i.putExtra("roomno","2");
+			i.putExtra("email",email);
+			i.putExtra("devid",devarray[1]);
+			startActivity(i);	
+		}
+		else if(arg0==iv[2])
+		{
+			Intent i=new Intent(this,roomdetailact.class);
+			System.out.println("status "+status[2]);
+			i.putExtra("status",status[2]);
+			i.putExtra("roomno","3");
+			i.putExtra("email",email);
+			i.putExtra("devid",devarray[2]);
+			startActivity(i);	
+		}
+		else if (arg0 == iv[3]) {
+            Intent i = new Intent(this, roomdetailact.class);
+            System.out.println("status " + status[3]);
+            i.putExtra("status", status[3]);
+            i.putExtra("roomno", "4");
+            i.putExtra("email", email);
+            i.putExtra("devid", devarray[3]);
+            startActivity(i);
+        }
+		else
+		{
+			Intent i=new Intent(this,roomdetailact.class);
+			System.out.println("status "+status[4]);
+			i.putExtra("status",status[4]);
+			i.putExtra("roomno","5");
+			i.putExtra("email",email);
+			i.putExtra("devid",devarray[4]);
+			startActivity(i);	
+		}
+		
+	}
 }
